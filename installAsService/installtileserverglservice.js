a51f0b96e04fa2c1708d5ebcfd0d35b615b825d6
Terry Woodruff <terry.woodruff@blackspacetechnology.com>
2024-01-14 11:32:08 +0000

    chore: updating config to behave. Adding installAsService script
    Signed-off-by: Terry Woodruff <terry.woodruff@blackspacetechnology.com>

---
+++ installAsService/installtileserverglservice.js
@@ -0,0 +1,25 @@
+// make sure you run npm install -g node-windows first
+// then link this directory with npm link node-windows
+// don't try allowServiceLogin, instead it will use localsystem, then you need to change it to 
+// networkservice, along with giving permissions for network service to access the relevant files
+
+
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'TileServer-gl',
+  description: 'TileServer-gl (native) Service.',
+  script: 'e:\\tileserver-gl\\src\\main.js',
+  scriptOptions: '--port 8082',  
+  workingDirectory: 'e:\\tileserver-gl'
+  //, allowServiceLogon: true -- set to network service using service.msc
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+});
+
+svc.install();
